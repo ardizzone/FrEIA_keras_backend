15bff30fc8abe5742c14b7c254eb59c6b2216d70
Lynton Ardizzone <lynton.ardizzone@iwr.uni-heidelberg.de>
2020-08-27 18:11:49 +0200

    subnetwork using keras sequential

---
+++ subnet.py
@@ -0,0 +1,21 @@
+import tensorflow.keras as kr
+
+class SubnetFactory:
+    def __init__(self, ch_hidden):
+        self.ch = ch_hidden
+
+    def __call__(self, ch_in, ch_out):
+        net = kr.models.Sequential([
+                kr.layers.Conv2D(self.ch, 3, padding='same'),
+                kr.layers.BatchNormalization(),
+                kr.layers.ReLU(),
+
+                kr.layers.Conv2D(self.ch, 3, padding='same'),
+                kr.layers.BatchNormalization(),
+                kr.layers.ReLU(),
+
+                kr.layers.Conv2D(ch_out, 3, padding='same'),
+            ])
+
+        return net
+
